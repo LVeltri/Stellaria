39ea46d23c68ab2ea161ec995d4fd6733b1b9f83
Lucas Veltri <veltri.lucas@live.fr>
2022-03-30 12:10:12 +0200

    generate auto wav file

--- audioCoding/audio.cpp
+++ audioCoding/audio.cpp
@@ -1,79 +1,101 @@
 #include <iostream>
 #include <cmath>
-#include <string>
 #include <fstream>
-#include <portaudio/portaudio.h>
+#include <sndfile.h>
+#include <sndfile.hh>
 
 using namespace std;
 
-const int sample_rate = 44100;
+#define M_PI 3.1415926535
+
+const int sampleRate = 44100;
 const int bitDepth = 16;
 
-#ifndef MPI
-#define MPI 3.141592653589
-#endif
+class SineOscillator {
+    float frequency, amplitude, angle = 0.0f, offset = 0.0f;
+    fstream file;
+public:
+    SineOscillator(float freq, float amp) : frequency(freq), amplitude(amp) {
+        offset = 2 * M_PI * frequency / sampleRate;
+    }
+    float process() {
+        auto sample = amplitude/2 * (sin(angle) + sin(angle * 0.5));
+        angle += offset;
+        return sample;
+        // Asin(2pif/sr)
+    }
 
-class Sine{
-    float frequency, amplitude, angle = 0.0f, offset = 0.0;
-    ofstream audioFile;
-    public:
-        Sine(){};
-        Sine(float freq, float amp) : frequency(freq), amplitude(amp){
-            offset = 2 * MPI * frequency / sample_rate;
+    void writeToFile(int value, int size){
+        file.write(reinterpret_cast<const char*> (&value), size);
+    }
+
+    void create_file(string filename, int duration){
+        file.open(filename, ios:binary);
+
+        // Header chunk
+        file << "RIFF";
+        file << "----";
+        file << "WAVE";
+
+        // format chunk
+        file << "fmt ";
+        writeToFile(16, 4); // Size
+        writeToFile(1, 2); // Compression code
+        writeToFile(1, 2); // Number of channels
+        writeToFile(sampleRate, 4); // Sample rate
+        writeToFile(sampleRate * bitDepth / 8, 4 ); // Byte rate
+        writeToFile(bitDepth / 8, 2); // Block align
+        writeToFile(bitDepth, 2); // Bit depth
+
+        // Data chunk
+        file << "data";
+        file << "----";
+
+        int preAudioPosition = file.tellp();
+
+        auto maxAmplitude = pow(2, bitDepth - 1) - 1;
+        for(int i = 0; i < sampleRate * duration; i++){
+            auto sample = process();
+            int intSample = static_cast<int> (sample * maxAmplitude);
+            writeToFile(intSample, 2);
         }
-        float process(){
-            auto sample =  amplitude * sin(angle);
-            angle += offset;
-            return sample;
-        }
-        void write(float duration){
 
-            auto maxAmplitude = pow(2, bitDepth -1) -1;
-            for(int i = 0; i < sample_rate * duration; i++){
-                auto sample = process();
-                int intSample = static_cast<int> (sample * maxAmplitude);
-                audioFile.write(reinterpret_cast<char*> (&intSample), 2);  
-            }
-                    
-        }
+        int postAudioPosition = file.tellp();
 
-        void changeFreq(int newFreq){
-            offset = 2 * MPI * newFreq / sample_rate;
-        }
+        file.seekp(preAudioPosition - 4);
+        writeToFile(postAudioPosition - preAudioPosition, 4);
 
+        file.seekp(4, ios::beg);
+        writeToFile(postAudioPosition - 8, 4);
 
+        file.close();
 
-        void read(float *frequency, float *duration, int size){
-            audioFile.open("waveform.wav", ios::binary);
-            for(int i = 0; i < size; i++){
-               changeFreq(frequency[i]);
-               write(duration[i]);
-           }
-           audioFile.close(); 
-        }
+    }
+
 };
 
-typedef int PaCallback(const void *input,
-                             void *output,
-                             unsigned long frameCount,
-                             const PaStreamCallbackTimeInfo* timeInfo,
-                             PaStreamCallbackFlags statusFlags,
-                             void *userData);
+static void read_file(const char* filename){
+    static short buffer[1024];
+    SndfileHandle file;
 
-int main(){
+    file = SndfileHandle(filename);
 
-    PaError err;
+    printf("opening file: %s", filename);
+    
+    file.read(buffer, 1024);
+    puts("");
 
-    err = Pa_Initialize();
+}
 
+int main() {
+    int duration = 2;
+    ofstream audioFile;
+    audioFile.open("waveform.wav", ios::binary);
+    SineOscillator sineOscillator(243,0.5);
 
-    float arrDuration[8] = {0.5,0.25,0.25,0.5,0.25,0.255,0.5,0.25};
-    float arrFreq[8] = {261.93, 392.00, 329.63, 392.00, 261.93, 392.00, 329.63, 392.00};
+    // Opening and reading file with libsndfile
 
-    Sine sine(440,0.2);
-    sine.read(arrFreq,arrDuration,8);
+    read_file("waveform.wav");
 
-
-    Pa_Terminate();
     return 0;
 }
